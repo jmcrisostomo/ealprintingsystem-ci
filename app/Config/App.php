c090067fb8c7e6089d6d7822a953e1be2601398c
JM Crisostomo <jm.crisostomo.e@gmail.com>
2022-06-01 17:32:40 +0800

    test for heroku app

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,8 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://ealprintingsystem-ci.test/';
+    // public $baseURL = 'https://ealprintingsystem-ci.test/';
+    public $baseURL = 'https://test-app-ci4.herokuapp.com/';
 
     /**
      * --------------------------------------------------------------------------
